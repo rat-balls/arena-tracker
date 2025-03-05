10ef50a682719198644f43682479147534ebd4d9
rat-balls <delalande.ethan@gmail.com>
2025-03-05 13:59:07 +0100

    Added status bar support

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,31 @@
 import {
   KeyboardAvoidingView,
   Platform,
-  ScrollView,
+  StatusBar,
   StyleSheet,
-  View,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import ChampionListComponent from "../components/championListComponent";
 
 export default function Index() {
   return (
-    <>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        enabled
-        style={{ flex: 1 }}
-      >
-        <LinearGradient colors={["#091428", "#0A1428"]} style={s.background} />
-        <ChampionListComponent />
-      </KeyboardAvoidingView>
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar backgroundColor="white" />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          enabled
+          style={{ flex: 1 }}
+        >
+          <LinearGradient
+            colors={["#091428", "#0A1428"]}
+            style={s.background}
+          />
+          <ChampionListComponent />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
