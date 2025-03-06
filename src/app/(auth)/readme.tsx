74564e6ba903e467e6a0e342ce138734703bb494
NoeHLM <noeheylemadec@gmail.com>
2025-03-06 19:20:38 +0100

    readme from github in markdown

---
+++ src/app/(auth)/readme.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import Markdown from "react-native-markdown-display";
+
+const markdownContent = `
+# Arena Tracker
+
+Arena Tracker is an application that allows League of Legends players to track the champions with whom they have won a game in the Arena game mode. This tracking is done using [Riot Games](https://developer.riotgames.com/docs/lol) API.
+
+## Built with
+
+
+### Expo
+
+
+## Team
+
+- Ethan DELALANDE
+- Jathurshan SIVANANTHAN
+- Noé HEY-LE MADEC
+
+## Get started
+
+#### Download the apk at (le chemin de l'apk)
+
+or
+
+#### Install locally
+
+1. Install dependencies
+
+   \`\`\`bash
+   npm install
+   \`\`\`
+   or
+
+   \`\`\`bash
+   yarn install
+   \`\`\`
+
+2. Start the app
+
+   \`\`\`bash
+    npx expo start
+   \`\`\`
+   
+3. Choose Expo Go or create development build
+
+- #### Expo Go
+scan the QR Code and launch on Expo Go (app)
+
+- #### Development build
+   \`\`\`bash
+  npx expo install expo-dev-client
+
+  eas build --profile development --platform android
+   \`\`\`
+
+## Features
+
+- Track winning champions in the Arena game mode.
+- Add accounts in favorite for tracking them (and import pictures for modify their profile picture).
+`;
+
+export default function Readme() {
+  return (
+    <ScrollView style={styles.container}>
+      <Markdown>{markdownContent}</Markdown>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+});
