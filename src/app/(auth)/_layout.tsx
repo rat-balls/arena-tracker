bd60f0b9d37935053ec0f8b16b0234f666bc8dba
NoeHLM <noeheylemadec@gmail.com>
2025-03-06 18:54:43 +0100

    add tabs routing

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,7 +1,29 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Layout = () => {
-  return <Stack />;
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="readme"
+        options={{
+          title: "Readme",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="file" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default Layout;
