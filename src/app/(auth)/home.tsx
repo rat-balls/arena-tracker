1941b1c11ec0060dc23af6c9f542f972ebe8dc0c
jathurshandev <jathurshandev@gmail.com>
2025-03-07 01:22:40 +0100

    clean home

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,17 +1,8 @@
-import { RelativePathString, useRouter } from "expo-router";
 import { Button, View } from "react-native";
 import { auth } from "../../firebase/firebase";
 export default function Page() {
-  const router = useRouter();
-
-  const account = "account" as RelativePathString;
-
   const signOut = () => {
     auth.signOut();
-  };
-
-  const redirectAccount = () => {
-    router.replace(account);
   };
 
   return (
